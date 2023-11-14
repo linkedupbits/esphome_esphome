3d5c3ad5853beb5ee76f617298dc23c4ee73f2b6
Stephen Cox <scox.nz@icloud.com>
2023-11-14 21:22:33 +1300

    included polling component into namespace

--- esphome/components/tft_espi/__init__.py
+++ esphome/components/tft_espi/__init__.py
@@ -24,7 +24,9 @@ from esphome.const import (
 )
 
 tft_espi_ns = cg.esphome_ns.namespace("tft_espi")
-TFT_ESPI = tft_espi_ns.class_("TFT_eSPI")
+TFT_ESPI = tft_espi_ns.class_("TFT_eSPI",
+ cg.PollingComponent
+)
 TFT_ESPIRef = TFT_ESPI.operator("ref")
 
 def validate_tft_espi(config):
