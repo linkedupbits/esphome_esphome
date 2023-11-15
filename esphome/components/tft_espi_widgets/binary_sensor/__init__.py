f00869fca2ecaab17f353cffae94f954081b94ed
Stephen Cox <scox.nz@icloud.com>
2023-11-15 22:11:31 +1300

    just needed to register teh external component

--- esphome/components/tft_espi_widgets/binary_sensor/__init__.py
+++ esphome/components/tft_espi_widgets/binary_sensor/__init__.py
@@ -1,25 +1,5 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, sensor
-from esphome.const import CONF_ID, ICON_EMPTY, UNIT_EMPTY
-
-DEPENDENCIES = ['uart']
-
-empty_uart_sensor_ns = cg.esphome_ns.namespace('tft_espi_widgets')
-EmptyUARTSensor = empty_uart_sensor_ns.class_('EmptyUARTSensor', cg.PollingComponent, uart.UARTDevice)
-
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 1).extend({
-    cv.GenerateID(): cv.declare_id(EmptyUARTSensor),
-}).extend(cv.polling_component_schema('60s')).extend(uart.UART_DEVICE_SCHEMA)
-
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-
-
-"""
-import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import binary_sensor
 from esphome.const import (
     CONF_ID,
@@ -54,4 +34,3 @@ async def to_code(config):
     #espi = await cg.get_variable(config[CONF_TFT_ESPI_ID])
     #var = cg.new_Pvariable(config[CONF_ID], espi)
     await cg.register_component(var, config)
-"""
