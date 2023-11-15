374dac15ee56668802025404f85f36cc9fc22c79
Stephen Cox <scox.nz@icloud.com>
2023-11-15 22:14:48 +1300

    Painful lack of documentation

--- esphome/components/tft_espi_widgets/binary_sensor/__init__.py
+++ esphome/components/tft_espi_widgets/binary_sensor/__init__.py
@@ -19,9 +19,9 @@ CONF_TFT_eSPI_ESPHome_Button_ID = "TFT_eSPI_Widget_Button"
 tft_espi_widgets_ns = cg.esphome_ns.namespace("tft_espi_widgets")
 TFT_eSPI_Button = tft_espi_widgets_ns.class_("TFT_eSPI_ESPHome_Button", binary_sensor.BinarySensor)
 
-CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(TFT_eSPI_Button).extend(
+CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(TM1637Key).extend(
     {
-        cv.GenerateID(CONF_TFT_eSPI_ESPHome_Button_ID), #: cv.use_id(TFT_ESPI),
+        cv.GenerateID(CONF_TFT_eSPI_ESPHome_Button_ID): cv.use_id(TFT_eSPI_Button),
     }
 )
 
@@ -32,5 +32,5 @@ async def to_code(config):
     cg.add_library("TFT_eSPI_Widgets", None)
     
     #espi = await cg.get_variable(config[CONF_TFT_ESPI_ID])
-    #var = cg.new_Pvariable(config[CONF_ID], espi)
+    var = cg.new_Pvariable(config[CONF_TFT_eSPI_ESPHome_Button_ID], espi)
     await cg.register_component(var, config)
