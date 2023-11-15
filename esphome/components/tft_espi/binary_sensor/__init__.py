6fb7394f292408ff20146bc72c6561c4f28aca12
Stephen Cox <scox.nz@icloud.com>
2023-11-15 23:00:31 +1300

    included component for schema

--- esphome/components/tft_espi/binary_sensor/__init__.py
+++ esphome/components/tft_espi/binary_sensor/__init__.py
@@ -30,6 +30,7 @@ CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(TFT_eSPI_Button).extend(
         cv.GenerateID(CONF_TFT_eSPI_ESPHome_Button_ID): cv.use_id(TFT_eSPI_Button),
         cv.GenerateID(CONF_TFT_ESPI_ID): cv.use_id(TFT_ESPI),
     }
+    .extend(cv.COMPONENT_SCHEMA)
 )
 
 # https://gite.lirmm.fr/doccy/TFT_eSPI_Widgets
