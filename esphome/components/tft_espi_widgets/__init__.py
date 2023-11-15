da4f1275681473a693bb757213060e9373f51117
Stephen Cox <scox.nz@icloud.com>
2023-11-15 21:42:30 +1300

    Moved binary_sensors into directory

--- esphome/components/tft_espi_widgets/__init__.py
+++ esphome/components/tft_espi_widgets/__init__.py
@@ -1,1 +1,6 @@
-# this file deliberately empty
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+from esphome.const import CONF_ID
+
+CODEOWNERS = ["@linkedupbits"]
--- esphome/components/tft_espi_widgets/binary_sensor/__init__.py
+++ esphome/components/tft_espi_widgets/binary_sensor/__init__.py
@@ -4,13 +4,13 @@ from esphome.const import (
     CONF_ID,
 )
 
-from ../tft_espi import (
-    CONF_TFT_ESPI_ID
-)
+#from ../tft_espi import (
+#    CONF_TFT_ESPI_ID
+#)
 
 CODEOWNERS = ["@linkedupbits"]
 
-DEPENDENCIES = ["tft_espi"]
+#DEPENDENCIES = ["tft_espi"]
 AUTO_LOAD = ["binary_sensor"]
 
 CONF_TFT_eSPI_ESPHome_Button_ID = "TFT_eSPI_Widget_Button"
@@ -20,7 +20,7 @@ TFT_eSPI_Button = tft_espi_widgets_ns.class_("TFT_eSPI_ESPHome_Button", binary_s
 
 CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(TFT_eSPI_Button).extend(
     {
-        cv.GenerateID(CONF_TFT_eSPI_ESPHome_Button_ID): cv.use_id(TFT_ESPI),
+        cv.GenerateID(CONF_TFT_eSPI_ESPHome_Button_ID), #: cv.use_id(TFT_ESPI),
     }
 )
 
@@ -30,6 +30,6 @@ async def to_code(config):
     cg.add_library("TFT_eSPI", None)
     cg.add_library("TFT_eSPI_Widgets", None)
     
-    espi = await cg.get_variable(config[CONF_TFT_ESPI_ID])
-    var = cg.new_Pvariable(config[CONF_ID], espi)
+    #espi = await cg.get_variable(config[CONF_TFT_ESPI_ID])
+    #var = cg.new_Pvariable(config[CONF_ID], espi)
     await cg.register_component(var, config)
