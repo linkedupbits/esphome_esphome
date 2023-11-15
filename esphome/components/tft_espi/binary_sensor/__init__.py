00426a4458f5ca060a1dec9ae3494dbe1aaa20b5
Stephen Cox <scox.nz@icloud.com>
2023-11-15 23:15:16 +1300

    playing with referenceing TFT_eSPI

--- esphome/components/tft_espi/binary_sensor/__init__.py
+++ esphome/components/tft_espi/binary_sensor/__init__.py
@@ -18,7 +18,7 @@ _LOGGER = logging.getLogger(__name__)
 _LOGGER.info("Test %s...", "a String")
 
 DEPENDENCIES = ["tft_espi"]
-AUTO_LOAD = ["binary_sensor"]
+# AUTO_LOAD = ["binary_sensor"]
 
 CONF_TFT_eSPI_ESPHome_Button_ID = "TFT_eSPI_Widget_Button"
 
@@ -30,7 +30,7 @@ CONFIG_SCHEMA = cv.All(
     .extend(cv.COMPONENT_SCHEMA)
     .extend(
         {
-            cv.GenerateID(CONF_TFT_eSPI_ESPHome_Button_ID): cv.use_id(TFT_eSPI_Button),
+            cv.GenerateID(CONF_TFT_ESPI_ID): cv.use_id(TFT_ESPI),
         }
     )
 )
@@ -42,5 +42,5 @@ async def to_code(config):
     cg.add_library("TFT_eSPI_Widgets", None)
     
     espi = await cg.get_variable(config[CONF_TFT_ESPI_ID])
-    var = cg.new_Pvariable(config[CONF_TFT_eSPI_ESPHome_Button_ID], espi)
+    var = cg.new_Pvariable(config, espi)
     await cg.register_component(var, config)
