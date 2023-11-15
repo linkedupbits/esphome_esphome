219d4a68825649b4ee52bbd0245884062835e219
Stephen Cox <scox.nz@icloud.com>
2023-11-15 21:13:30 +1300

    referenced CONF_TFT_ESPI_ID

--- esphome/components/tft_espi_widgets/__init__.py
+++ esphome/components/tft_espi_widgets/__init__.py
@@ -4,9 +4,15 @@ from esphome.const import (
     CONF_ID,
 )
 
+from ../tft_espi import (
+    CONF_TFT_ESPI_ID
+)
+
 CODEOWNERS = ["@linkedupbits"]
 
 DEPENDENCIES = ["tft_espi"]
+AUTO_LOAD = ["binary_sensor"]
+
 CONF_TFT_eSPI_ESPHome_Button_ID = "TFT_eSPI_Widget_Button"
 
 tft_espi_widgets_ns = cg.esphome_ns.namespace("tft_espi_widgets")
@@ -24,6 +30,6 @@ async def to_code(config):
     cg.add_library("TFT_eSPI", None)
     cg.add_library("TFT_eSPI_Widgets", None)
     
-    espi = await cg.get_variable(config[CONF_NEXTION_ID])
+    espi = await cg.get_variable(config[CONF_TFT_ESPI_ID])
     var = cg.new_Pvariable(config[CONF_ID], espi)
     await cg.register_component(var, config)
