d99544a1c25569eb9db63e92db1985921485aa74
Stephen Cox <scox.nz@icloud.com>
2023-11-17 21:39:57 +1300

    different path to file?

--- esphome/components/tft_espi_sprite/TFT_eSprite_esphome.h
+++ esphome/components/tft_espi_sprite/TFT_eSprite_esphome.h
@@ -8,7 +8,7 @@
 #include <vector>
 #include "TFT_eSPI.h"              // Hardware-specific library
 #include "TFT_eWidget.h"           // Widget library
-#include "../tft_espi/tft_espi.h"
+#include "esphome/components/tft_espi/tft_espi.h"
 
 //#define BUTTON_W 100
 //#define BUTTON_H 50
