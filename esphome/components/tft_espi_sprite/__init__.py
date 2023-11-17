cc3eae4ae76498c2e45dd20f80b6d0b83679ccae
Stephen Cox <scox.nz@icloud.com>
2023-11-17 21:34:51 +1300

    included libraries

--- esphome/components/tft_espi_sprite/__init__.py
+++ esphome/components/tft_espi_sprite/__init__.py
@@ -49,6 +49,9 @@ CONFIG_SCHEMA = cv.Schema(
 ).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
+    cg.add_library("TFT_eSPI", None)
+    cg.add_library("TFT_eWidget", None)
+
     espi = await cg.get_variable(config[CONF_TFT_ESPI_ID])
     var = cg.new_Pvariable(config[CONF_ID], espi)
     await cg.register_component(var, config)
