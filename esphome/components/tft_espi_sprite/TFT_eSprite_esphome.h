89ad801ec38fcdbe2c6a5962c9668ad9f0344578
Stephen Cox <scox.nz@icloud.com>
2023-11-17 23:24:02 +1300

    trying...

--- esphome/components/tft_espi_sprite/TFT_eSprite_esphome.h
+++ esphome/components/tft_espi_sprite/TFT_eSprite_esphome.h
@@ -22,7 +22,7 @@ namespace tft_espi_sprite {
             TFT_eSPI* tft;
 
         public:
-            TFT_eSprite_ESPHome(void** /*tft_espi::TFT_eSPI_ESPHome**/ owner) {
+            TFT_eSprite_ESPHome(esphome::tft_espi::TFT_eSPI_ESPHome*& owner) {
                 //this->tft = owner->TFTptr();
             }
             void setup() override;
