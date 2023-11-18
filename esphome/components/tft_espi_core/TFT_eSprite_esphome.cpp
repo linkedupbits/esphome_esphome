5134dea60ca17efd6cc08797e902cc50b85bc4e4
scox.nz@icloud.com <scox.nz@icloud.com>
2023-11-18 08:19:14 +0000

    removed test loging

--- esphome/components/tft_espi_core/TFT_eSprite_esphome.cpp
+++ esphome/components/tft_espi_core/TFT_eSprite_esphome.cpp
@@ -7,11 +7,11 @@ namespace tft_espi_sprite {
 static const char *const TAG = "TFT_eSprite_esphome";
 
 void TFT_eSprite_ESPHome::setup() {
-    ESP_LOGD(TAG, "setup");
+    //ESP_LOGD(TAG, "setup");
 }
 
 void TFT_eSprite_ESPHome::loop() {
-    ESP_LOGD(TAG, "loop");
+    //ESP_LOGD(TAG, "loop");
 }
 
 }
